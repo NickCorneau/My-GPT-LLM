41089b3129004966fb2d0979e3c348430d3c5488
Nicholas Corneau <nicholascorneau@gmail.com>
2025-04-06 17:56:06 -0400

    We can now run the tensor code

--- tensor-tokenizer.py
+++ tensor-tokenizer.py
@@ -1,5 +1,6 @@
+import tiktoken
 import torch
-from torch.utils.data import Dataset, Dataloader
+from torch.utils.data import Dataset, DataLoader
 
 class GPTDatasetV1(Dataset):
     def __init__(self, txt, tokenizer, max_length, stride):
@@ -18,13 +19,13 @@ class GPTDatasetV1(Dataset):
     def __len__(self):
         return len(self.input_ids)
 
-    def __row__(self, idx):
-        return self.inputs_ids[idx], self.target_ids[idx]
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
 
 def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
     tokenizer = tiktoken.get_encoding("gpt2")
     dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
-    dataloader = Dataloader(
+    dataloader = DataLoader(
                 dataset,
                 batch_size=batch_size,
                 shuffle=shuffle,
@@ -41,7 +42,7 @@ def read_file(relative_file_path) -> str:
 
 raw_text = read_file("data/the-verdict.txt")
 dataloader = create_dataloader_v1(
-            raw_text, batch_size=1, max_length=4, stride=1, shuffle=False
+        raw_text, batch_size=1, max_length=6, stride=4, shuffle=False
         )
 data_iter = iter(dataloader)
 first_batch = next(data_iter)
